601e4cf0bc2d6e91819588f25ee00c3d90444e84
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-06-13 22:59:17 +0100

    Add pricing page

---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Pricing = () => {
+  return (
+    <div>
+        <Navbar />
+        <p className="font-semibold p-4 text-2xl sm:5xl mt-24 w-fit mx-auto text-primary">We offer very flexible pricing models</p>
+    </div>
+  )
+}
+
+export default Pricing
