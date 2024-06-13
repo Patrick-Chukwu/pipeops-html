a6b46e4193abe85adbe99fdffe764d959f9a75cf
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-06-13 22:54:44 +0100

    Add file

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Blog = () => {
+  return (
+    <div className='mt-24'>
+        <Navbar/>
+        <p className='font-semibold w-fit mx-auto text-2xl sm:text-5xl p-4 text-primary'>Welcome to our Blog</p>
+    </div>
+  )
+}
+
+export default Blog
